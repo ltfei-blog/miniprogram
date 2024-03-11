1958da524fdcd860ccfe59e141ce5752177b19d5
ltfei <ltfei@ltfei.cn>
2024-03-11 09:38:26 +0800

    feat(pages/my/my): 用户页跳转逻辑

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 import './my.less'
 import { userStore } from '../../stores/user'
-import { Image } from '@tarojs/components'
+import { Image, Navigator } from '@tarojs/components'
+import scanCodeImage from '../../static/scan-code_1.png'
+import userImage from '../../static/user.png'
+import Taro from '@tarojs/taro'
 
 defineOptions({
   name: 'my'
 })
 
 const { user } = userStore()
+
+/**
+ * todo: 扫码逻辑
+ */
+const scan = async () => {
+  await Taro.scanCode({})
+}
 </script>
-<!-- todo: 获取用户信息，页面底部按钮  -->
+<!-- todo: 页面底部按钮  -->
 <template>
   <div class="my">
     <div class="background-top">
@@ -24,22 +34,41 @@ const { user } = userStore()
             <div class="desc">简介...</div>
           </div>
         </div>
-        <div class="scan"></div>
+        <div class="scan" @click.stop="scan">
+          <Image class="image" :src="scanCodeImage" />
+        </div>
       </div>
       <!-- 未登录 -->
-      <div class="user" v-else>
-        <div class="userinfo">
+      <div class="user not-login" v-else>
+        <Navigator url="/subpkg/login/login" class="userinfo">
           <div class="avatar">
-            <!-- <Image class="image" :src="" /> -->
+            <Image class="image" :src="userImage" />
           </div>
           <div class="text">
             <div class="username">请登录</div>
             <div class="desc">简介...</div>
           </div>
+        </Navigator>
+        <div class="scan" @click.stop="scan">
+          <Image class="image" :src="scanCodeImage" />
         </div>
-        <div class="scan"></div>
       </div>
     </div>
-    <div class="background-bottom"></div>
+    <div class="background-bottom">
+      <div class="data">
+        <div class="item">
+          <div class="value">28</div>
+          <div class="name">作品</div>
+        </div>
+        <div class="item">
+          <div class="value">28</div>
+          <div class="name">获赞</div>
+        </div>
+        <div class="item">
+          <div class="value">28</div>
+          <div class="name">粉丝</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
